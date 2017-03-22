ef4c587e13e056402e624b128427a35b6deed2ef
AvinashSingh786 <u14043778@tuks.co.za>
2017-03-22 23:25:13 +0200

    added login page files

---
+++ NavUP/src/pages/login/login.ts
@@ -0,0 +1,17 @@
+/**
+ * Created by Avinash on 2017-03-22.
+ */
+import { Component } from '@angular/core';
+
+
+
+@Component({
+    templateUrl: 'login.html'
+})
+export class LoginPage {
+
+
+    constructor() {
+
+    }
+}
