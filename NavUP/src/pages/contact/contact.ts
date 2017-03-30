12f63b28ebb74f963b5e762432ba98a34e01bc60
AvinashSingh786 <u14043778@tuks.co.za>
2017-03-30 22:57:19 +0200

    Wrote a quick test to check if the ajax handler is working

--- NavUP/src/pages/contact/contact.ts
+++ NavUP/src/pages/contact/contact.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {API} from "../../util/API";
 
 @Component({
   selector: 'page-contact',
@@ -8,8 +9,16 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+  api: API;
+  data:any;
 
+  constructor(public navCtrl: NavController, api:API) {
+    this.api = api;
   }
 
+  ngOnInit() {
+    this.api.get("/latest?base=USD").subscribe(
+        data => this.data = JSON.stringify(data)
+    );
+  }
 }
