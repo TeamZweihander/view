76fa637d2e4faaad38d5b7908ce18328961135ad
RuanKlinkert <klinkert.ruan@gmail.com>
2017-04-17 17:40:46 +0200

    I added the modal launch code to the tab bar

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
-import {LoginPage} from "../login/login";
-import {RegisterPage} from "../register/register";
+import { LoginPage} from "../login/login";
+import { RegisterPage } from "../register/register";
 
-import { ModalController } from 'ionic-angular';
+import { ModalController, NavController } from 'ionic-angular';
 import { SearchPage } from '../search/search';
 
 @Component({
@@ -17,12 +17,11 @@ export class TabsPage {
   tab2Root: any = LoginPage;
   tab3Root: any = RegisterPage;
 
-  constructor(public modalCtrl: ModalController) {
+  constructor(public modalCtrl: ModalController, public nav: NavController) {
     
   }
 
   presentModal() {
-    alert("dd");
     let modal = this.modalCtrl.create(SearchPage);
     modal.onDidDismiss(data => {alert(JSON.stringify(data))});
     modal.present();
