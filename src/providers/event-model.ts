0574a97e1aa3379675cc718294383c58ff216472
RuanKlinkert <u14022282@tuks.co.za>
2017-04-19 15:31:58 +0200

    Revert "Revert "Feature/events""

---
+++ src/providers/event-model.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {DateModel} from "./date-model";
+import {TimeModel} from "./time-model";
+import {LocationModel} from "./location-model";
+
+/*
+  Generated class for the EventModel provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class EventModel {
+
+  public id: number;
+  public title: string;
+  public description: string;
+  public image: string;
+  public date: DateModel;
+  public to: TimeModel;
+  public from: TimeModel;
+  public destination: LocationModel;
+}
