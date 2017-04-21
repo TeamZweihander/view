c74c7c78bff73acd7ff9519ec8517df1d7e73f1f
RuanKlinkert <klinkert.ruan@gmail.com>
2017-04-21 13:30:46 +0200

    Updated usermodel, auth-service, and profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import {Component, ViewChild} from '@angular/core';
 import {Nav, Platform} from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
 import { TabsPage } from '../pages/tabs/tabs';
 import {API} from "../util/API";
 import {LoginPage} from "../pages/login/login";
 import {RegisterPage} from "../pages/register/register";
 import {HomePage} from "../pages/home/home";
 import {EventsListPage} from "../pages/events-list/events-list";
+import { AuthService } from "../providers/auth-service";
 
 @Component({
   templateUrl: 'app.html',
@@ -19,21 +19,35 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage = HomePage;
   pages: Array<{title: string, component: any}>;
-  avatar = "assets/images/user_avatar.svg";
-  name = "Guest";
+  avatar = "";
+  name = "";
+  email = "";
+  isLoggedIn = false;
+  currentUser: UserModel;
 
-
-  constructor(platform: Platform) {
+  constructor(platform: Platform, authService: AuthService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
-    this.pages = [
-      { title: 'Login', component: LoginPage },
-      { title: 'Register', component: RegisterPage },
-      { title: 'Events', component:  EventsListPage }];
+    if(authService.isAuthenticated())
+    {
+      this.avatar = "assets/images/user_avatar.svg";
+      this.name = "Guest";
+      this.email = "";
+      this.isLoggedIn = true;
+      this.pages = [
+        { title: 'Events', component:  EventsListPage }];
+    }
+    else
+    {
+      this.avatar = "assets/images/user_avatar.svg";
+      this.name = "Guest";
+      this.email = "";
+      this.pages = [{ title: 'Login', component: LoginPage }];
+    }
   }
 
   openPage(page) {
