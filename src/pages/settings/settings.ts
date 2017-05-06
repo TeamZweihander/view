4804d95e68f4dd5739ca18c9a857db2e192a3b00
AvinashSingh786 <u14043778@tuks.co.za>
2017-05-06 23:39:35 +0200

    added map settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {NavController, NavParams} from "ionic-angular";
-import {HomePage} from "../home/home";
+import { NativeStorage } from 'ionic-native';
+import { ToastController } from 'ionic-angular';
 /**
  * Created by Avinash on 2017/05/06.
  */
@@ -9,10 +10,68 @@ import {HomePage} from "../home/home";
   templateUrl: "settings.html"
 })
 export class SettingsPage{
-
-  constructor(public navCtrl: NavController, public navParams: NavParams)
+  maps: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController)
   {
 
+    NativeStorage.getItem('settings')
+      .then(
+        data => {
+          this.maps = data.maps;
+        },
+        error => {
+          NativeStorage.setItem('settings', {maps: 'road' }).then(
+            () => {this.maps = true;},
+            error =>
+            {
+              this.toastCtrl.create({
+                message: 'Failed to retrieve your map settings',
+                showCloseButton: true,
+                closeButtonText: 'Ok'
+              }).present();
+            }
+          );
+        }
+      );
   }
 
+  mapType() {
+
+    if (this.maps) {
+
+      NativeStorage.setItem('settings', {maps: 'road' }).then(
+        () => {
+          this.toastCtrl.create({
+            message: 'Successfully stored your map settings.',
+            duration: 2000
+          }).present();
+        },
+        error => {
+          this.toastCtrl.create({
+            message: 'Failed to update your map settings.',
+            showCloseButton: true,
+            closeButtonText: 'Ok'
+          }).present();
+        }
+      );
+    }
+    else {
+
+      NativeStorage.setItem('settings', {maps: 'sat' }).then(
+        () => {
+          this.toastCtrl.create({
+            message: 'Successfully stored your map settings.',
+            duration: 2000
+          }).present();
+        },
+        error => {
+          this.toastCtrl.create({
+            message: 'Failed to update your map settings.',
+            showCloseButton: true,
+            closeButtonText: 'Ok'
+          }).present();
+        }
+      );
+    }
+  }
 }
