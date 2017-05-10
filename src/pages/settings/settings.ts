81863529b7a691f58f59456ca56794d3e7ec30b4
AvinashSingh786 <u14043778@tuks.co.za>
2017-05-10 17:13:43 +0200

    Notifications settings now only work once user is logged in

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -3,6 +3,7 @@ import {NavController, NavParams} from "ionic-angular";
 // import { NativeStorage } from 'ionic-native';
 import { ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+import {AuthService} from "../../providers/auth-service";
 
 /**
  * Created by Avinash on 2017/05/06.
@@ -12,15 +13,24 @@ import { Storage } from '@ionic/storage';
   templateUrl: "settings.html"
 })
 export class SettingsPage{
-  maps: any = 'road';
+  maps: boolean = true;
+  email: boolean = true;
+  sms: boolean = false;
+  push: boolean = false;
+  isLoggedIn: boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, private storage: Storage)
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public toastCtrl: ToastController, private storage: Storage, private auth: AuthService)
   {
+    this.isLoggedIn = this.auth.isAuthenticated();
     this.storage.ready().then(() => {
       this.storage.get('settings').then((val) => {
         this.maps = val.maps == 'road';
+        this.email = val.email;
+        this.sms = val.sms;
+        this.push = val.push;
       }).catch((err) => {
-        this.storage.set('settings', {maps: 'road'}).then().catch((err) => {
+        this.storage.set('settings', {maps: 'road', email: true, sms: false, push: false}).then().catch((err) => {
           this.toastCtrl.create({
             message: 'Failed to retrieve your map settings',
             showCloseButton: true,
@@ -54,7 +64,7 @@ export class SettingsPage{
 
     if (this.maps) {
       this.storage.ready().then(() => {
-          this.storage.set('settings', {maps: 'road'}).then(
+          this.storage.set('settings', {maps: 'road', email: this.email, sms: this.sms, push: this.push}).then(
             () => {
               // this.toastCtrl.create({
               //   message: 'Successfully updated map settings to ROAD',
@@ -69,31 +79,15 @@ export class SettingsPage{
             }).present();
           });
         });
-
-      // NativeStorage.setItem('settings', {maps: 'road' }).then(
-      //   () => {
-      //     this.toastCtrl.create({
-      //       message: 'Successfully stored your map settings.',
-      //       duration: 2000
-      //     }).present();
-      //   },
-      //   error => {
-      //     this.toastCtrl.create({
-      //       message: 'Failed to update your map settings.',
-      //       showCloseButton: true,
-      //       closeButtonText: 'Ok'
-      //     }).present();
-      //   }
-      // );
     }
     else {
       this.storage.ready().then(() => {
-        this.storage.set('settings', {maps: 'sat'}).then(
+        this.storage.set('settings', {maps: 'sat', email: this.email, sms: this.sms, push: this.push}).then(
           () => {
-            this.toastCtrl.create({
-              message: 'Successfully updated map settings',
-              duration: 3000
-            }).present();
+            // this.toastCtrl.create({
+            //   message: 'Successfully updated map settings',
+            //   duration: 3000
+            // }).present();
           }
         ).catch((err) => {
           this.toastCtrl.create({
@@ -119,5 +113,25 @@ export class SettingsPage{
       //   }
       // );
     }
+  }
+
+  notificationUpdate() {
+    let tmp = this.maps;
+      this.storage.ready().then(() => {
+        this.storage.set('settings', {maps: tmp, email: this.email, sms: this.sms, push: this.push}).then(
+          () => {
+            // this.toastCtrl.create({
+            //   message: 'Successfully updated notification settings',
+            //   duration: 3000
+            // }).present();
+          }
+        ).catch((err) => {
+          this.toastCtrl.create({
+            message: 'Failed to update your notification settings',
+            showCloseButton: true,
+            closeButtonText: 'Ok'
+          }).present();
+        });
+      });
   }
 }
